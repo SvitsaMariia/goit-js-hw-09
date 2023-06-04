5c021280a31f7e93ebe373e32b455bda4e1b6516
Svitsa Mariia <mariia0198@gmail.com>
2023-06-05 02:21:05 +0300

    commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,97 @@
+import Notiflix from "notiflix"; 
 import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from "notiflix";
+import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/themes/material_blue.css';
+
+
+const dateSelector = document.getElementById('datetime-picker');
+const btnStart = document.querySelector('button[data-start]');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minuteEl = document.querySelector('span[data-minute]');
+const secondsEl = document.querySelector('span[data-seconds]');
+
+let selectedTime = 0;
+
+btnStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates && selectedDates.length > 0) {
+            selectedTime = Date.parse(selectedDates[0]);
+            checkDate();
+        } else {
+            Notiflix.Notify.failure("Please choose a date in the future");
+            btnStart.disabled = true;
+      }
+  },
+};
+const flatPic = flatpickr(dateSelector, options);
+
+function checkDate() {
+  if (Date.now() >= selectedTime) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    btnStart.disabled = true;
+  } else {
+    btnStart.disabled = false;
+    Notiflix.Notify.success('Starting countdown');
+  }
+}
+
+
+
+function onStartClick() {
+    btnStart.disabled = true;
+        dateSelector.disabled = true;
+  const timer = setInterval(() => {
+
+
+    const remainingTime = convertMs(selectedTime - Date.now());
+
+    if (remainingTime.seconds < 0) {
+      clearInterval(timer);
+      Notiflix.Report.success('Out of time!', 'Timer reached end.', 'OK');
+      dateSelector.disabled = false;
+    } else {
+      renderTimer(remainingTime);
+    }
+  }, 1000);
+}
+
+function addZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function renderTimer({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addZero(days);
+  hoursEl.textContent = addZero(hours);
+  minuteEl.textContent = addZero(minutes);
+  secondsEl.textContent = addZero(seconds);
+}
+
+//  console.log(selectedDates[0]);
+
+
+ function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minutes);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if(shouldResolve){
+        resolve ({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  })
+
+}
+
+const form = document.querySelector ('form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const delayInput = evt.target.elements.delay;
+  const stepInput = evt.target.elements.step;
+  const amountInput = evt.target.elements.amount;
+
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount= Number(amountInput.value);
+
+  for (let i = 1; i <= amount; i++){
+    createPromise(i, delay +(i - 1) * step)
+    .then(({position, delay}) => {
+      Notiflix.Notify.success(`Promise ${position} resolved after ${delay} ms`);
+    })
+    .catch (({position, delay}) => {
+      Notiflix.Notify.failure(`Promise ${position} rejected after ${delay} ms`);
+    })
   }
 }
