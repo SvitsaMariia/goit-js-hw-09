01a3db29ddf6cb0b667829ee0654c88d1fe6aa7d
SvitsaMariia <122281425+SvitsaMariia@users.noreply.github.com>
2023-06-04 13:53:43 +0000

    Deploying to gh-pages from @ SvitsaMariia/goit-js-hw-09@7f9e1e4bdf75c2858ecf74bcc1f6bc3a0f2d9b2e 🚀

---
+++ 01-color-switcher.e0a2d301.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),a.addEventListener("click",(function(){clearInterval(t),a.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.e0a2d301.js.map
